43a6cb32273158563c0150487ea15e98d32a0cb9
Chad Austin <chad@chadaustin.me>
2014-07-03 13:57:27 -0700

    demo repository

--- oldcomp.cpp
+++ oldcomp.cpp
@@ -1,1 +1,10 @@
-extern 
+#include <gmtl/MatrixOps.h>
+
+using namespace gmtl;
+
+extern "C" void matrixMultiply(Matrix44f& out, Matrix44f& lhs, Matrix44f& rhs) {
+    mult(out, lhs, rhs);
+}
+
+int main() {
+}
